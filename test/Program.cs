fd36a4dc8cfd4a00903a15f5ecf05b82fca256fd
AykhanLine <ayxankerimli2001@mail.ru>
2022-04-05 17:01:17 +0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string currentName = "Ayxan";
+string currentPassword = "ayxan2001";
+int currentCode = 1234;
+int currentValue = 500;
+
+Console.WriteLine("Adinizi yazin");
+string name = Console.ReadLine();
+
+Console.WriteLine("Sifrenizi yazin");
+string password = Console.ReadLine();
+
+Console.WriteLine("Kodunuzu yazin");
+string code = Console.ReadLine();
+
+if (name == currentName && password == currentPassword) ;
+{
+    Console.WriteLine("davam edin");
+};
+else
+{
+    Console.WriteLine("Melumat sefdi");
+};
+
